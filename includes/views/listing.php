5a95a6b8fd9f5c5c268f8dceaf290f98a097484a
Valerio Souza <valeriosza@gmail.com>
2017-04-04 18:57:00 -0700

    table

--- includes/views/listing.php
+++ includes/views/listing.php
@@ -33,19 +33,9 @@ if ( ! defined( 'WPINC' ) ) {
                     <input id="cb-select-all-1" type="checkbox">
                 </td>
                 <th scope="col" id="title" class="manage-column column-title column-primary sortable desc">
-                    <span>Título</span>
+                    <span> <?php echo __( 'Rule Name', 'notify-users-e-mail' ); ?></span>
                 </th>
-                <th scope="col" id="author" class="manage-column column-author">Autor</th>
-                <th scope="col" id="categories" class="manage-column column-categories">Categorias</th>
-                <th scope="col" id="tags" class="manage-column column-tags">Tags</th>
-                <th scope="col" id="comments" class="manage-column column-comments num sortable desc">
-                    <span><span class="vers comment-grey-bubble" title="Comentários">
-                        <span class="screen-reader-text">Comentários</span>
-                    </span></span>
-                </th>
-                <th scope="col" id="date" class="manage-column column-date sortable asc">
-                   <span>Data</span>
-                </th>
+                <th scope="col" id="categories" class="manage-column column-categories"><?php echo __( 'Type', 'notify-users-e-mail' ); ?></th>
             </tr>
         </thead>
         <tbody id="the-list">
@@ -58,28 +48,15 @@ if ( ! defined( 'WPINC' ) ) {
                     <span class="screen-reader-text">“Hello world!” está bloqueado</span>
                 </div>
                 </th>
-                <td class="title column-title has-row-actions column-primary page-title" data-colname="Título">
+                <td class="title column-title has-row-actions column-primary page-title" data-colname="<?php echo __( 'Rule Name', 'notify-users-e-mail' ); ?>">
                 <div class="locked-info">
                     <span class="locked-avatar"></span> <span class="locked-text"></span>
                 </div>
                 <strong>
                     <a class="row-title" href="<?php echo get_admin_url(); ?>admin.php?page=notify-users-e-mail-settings" aria-label="“Hello world!” (Editar)">Hello world!</a>
                 </strong>
-                <td class="author column-author" data-colname="Autor">
-                    <a href="edit.php?post_type=post&amp;author=1">admin</a>
-                </td>
-                <td class="categories column-categories" data-colname="Categorias">
+                <td class="categories column-categories" data-colname="<?php echo __( 'Type', 'notify-users-e-mail' ); ?>">
                     <a href="edit.php?category_name=uncategorized">Uncategorized</a>
-                </td>
-                <td class="tags column-tags" data-colname="Tags">
-                    <span aria-hidden="true">—</span><span class="screen-reader-text">Nenhuma tag</span>
-                </td>
-                <td class="comments column-comments" data-colname="Comentários">        
-                    <div class="post-com-count-wrapper">
-                    <a href="http://localhost/wordpress/wp-admin/edit-comments.php?p=1&amp;comment_status=approved" class="post-com-count post-com-count-approved"><span class="comment-count-approved" aria-hidden="true">1</span><span class="screen-reader-text">1 comentário</span></a><span class="post-com-count post-com-count-pending post-com-count-no-pending"><span class="comment-count comment-count-no-pending" aria-hidden="true">0</span><span class="screen-reader-text">Sem comentários pendentes</span></span>       </div>
-                </td>
-                <td class="date column-date" data-colname="Data">Publicado<br>
-                    <abbr title="04/04/2017 18:56:45">5 horas atrás</abbr>
                 </td>
             </tr>
         </tbody>
@@ -89,19 +66,10 @@ if ( ! defined( 'WPINC' ) ) {
                     <label class="screen-reader-text" for="cb-select-all-2">Selecionar todos</label><input id="cb-select-all-2" type="checkbox">
                 </td>
                 <th scope="col" class="manage-column column-title column-primary sortable desc">
-                   <span>Título</span>
+                   <span> <?php echo __( 'Rule Name', 'notify-users-e-mail' ); ?></span>
                 </th>
-                <th scope="col" class="manage-column column-author">Autor</th>
-                <th scope="col" class="manage-column column-categories">Categorias</th>
-                <th scope="col" class="manage-column column-tags">Tags</th>
-                <th scope="col" class="manage-column column-comments num sortable desc">
-                    <span><span class="vers comment-grey-bubble" title="Comentários">
-                        <span class="screen-reader-text">Comentários</span></span>
-                    </span>
-                </th>
-                <th scope="col" class="manage-column column-date sortable asc">
-                    <span>Data</span>
-                </th>   
+                <th scope="col" class="manage-column column-categories"><?php echo __( 'Type', 'notify-users-e-mail' ); ?></th>
+                  
             </tr>
         </tfoot>
     </table>

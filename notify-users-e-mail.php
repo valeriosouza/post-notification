532d811a0c9ba4d38f369b43af9694447c9e409b
Nitesh Kesarkar <niteshkesarkar136@gmail.com>
2020-05-29 22:59:05 +0530

    Issue #99 Task: Add support for {excerpt_post} in email

--- notify-users-e-mail.php
+++ notify-users-e-mail.php
@@ -288,7 +288,8 @@ class Notify_Users_EMail {
 	protected function apply_content_placeholders( $string, $post ) {
 		$string = str_replace( '{title}', sanitize_text_field( get_the_title( $post->ID ) ), $string );
 		$string = str_replace( '{link_post}', esc_url( get_permalink( $post->ID ) ), $string );
-		$string = str_replace( '{content_post}', apply_filters( 'the_content',get_post_field('post_content', $post->ID)), $string );
+		$string = str_replace( '{content_post}', apply_filters( 'the_content', get_post_field('post_content', $post->ID)), $string );
+		$string = str_replace( '{excerpt_post}', apply_filters( 'the_content', get_post_field('post_excerpt', $post->ID)), $string );
 		$string = str_replace( '{date}', $this->get_formated_date( $post->post_date ), $string );
 
 		return $string;
